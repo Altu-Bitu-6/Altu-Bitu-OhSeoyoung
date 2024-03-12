9f7e8a2326b47a87fcf5dd6f77b4de2a1f709d60
Seoyoung Oh <ohseoyoung5@gmail.com>
2024-03-12 09:37:06 +0900

    [브루트포스] 3월 12일

--- 04_브루트포스/11723.cpp
+++ 04_브루트포스/11723.cpp
@@ -1,12 +1,55 @@
-// 비트마스킹, 구현
-
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <string>
+#include <sstream>
 
+using namespace std;
 
 int main() {
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int m;
+    cin >> m;
+
+    string str;
+    int n;
+
+    int s = 0;
+
+    while(m--){
+        cin >> str;
+
+        if (str == "all"){
+            s = (1<<21)-1;
+        } else if (str == "empty") { 
+            s = 0;
+        } else {
+
+            cin >> n;
+
+            if (str == "add"){
+                s |= (1<<n);
+                
+            } else if (str == "remove"){
+                s &= ~(1<<n);
+            } else if (str == "check") {
+                if(s & (1<<n)){
+                    cout << "1\n";
+                } else {
+                    cout << "0\n";
+                }
+            } else { // str == "toggle"
+                if(s & (1<<n)){
+                    s &= ~(1<<n);
+                } else {
+                    s |= (1<<n);
+                }
+            } 
+        }
+    }
 
     return 0;
 }
